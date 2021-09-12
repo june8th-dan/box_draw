c9a347343adba8c20368dbd485819eea51ea784d
Le Dang Ngo Dan <69025063+danblaze-06@users.noreply.github.com>
2021-09-12 18:45:01 +0700

    Update box_draw.cpp - Fixed issue #1 and #2
    The excutable file will be updated soon...

--- box_draw.cpp
+++ box_draw.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
-#include <stdlib.h>
 using namespace std;
 
+//Character counting function
 int symbol(int w, int h){
-    int symbol = (w + h) * 2;
+    int symbol = (w + h) * 2 - 4;
     return symbol;
 }
 
 int main(){
     int width;
     int height;
-    cout << "Box drawer - By Danblaze" << endl << "Version 1.0.0" << endl << endl;
+    cout << "Box drawer - By Danblaze" << endl << "Version 1.0.1" << endl << endl;
     bool in_state = false;
     while(in_state == false){
         cout << "Enter number of width (Minimium: 3): "; cin >> width; cout << endl;
         cout << "Enter number of height (Minimium: 3): "; cin >> height; cout << endl;
+        //Pre-processor
         if(width >= 3){
             if(height >= 3){
                 in_state = true;
@@ -28,6 +29,7 @@ int main(){
         }        
     }
     cout << endl << "Drawing..." << endl << endl;
+    //Master function: Drawer
     for(int w = 0; w < width; w ++){
         cout << "* ";
     }
@@ -43,6 +45,8 @@ int main(){
         cout << "* ";
     }
     cout << endl << endl << "The box has been drawn by " << symbol(width, height) << " characters" << endl << endl;
-    system ("pause");
+    int done;
+    cout << "Press Enter to exit...";
+    cin >> done;
     return 0;
 }
